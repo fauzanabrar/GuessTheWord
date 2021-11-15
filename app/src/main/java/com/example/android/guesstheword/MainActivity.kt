ab86b6f502fbef9cd27aa2d112924215b1a2751e
Muhammad Fauzan Abrar <=>
2021-11-15 09:31:14 +0800

    add viewModel in GameFragment and ScoreFragment

--- app/src/main/java/com/example/android/guesstheword/MainActivity.kt
+++ app/src/main/java/com/example/android/guesstheword/MainActivity.kt
@@ -18,15 +18,21 @@ package com.example.android.guesstheword
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
+import com.example.android.guesstheword.screens.game.GameViewModel
 
 /**
  * Creates an Activity that hosts all of the fragments in the app
  */
 class MainActivity : AppCompatActivity() {
 
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
+
+
     }
 
 }
